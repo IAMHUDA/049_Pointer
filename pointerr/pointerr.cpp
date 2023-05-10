a8345597aab913e47e4f5375ab54375a92fe7d36
IAMHUDA <hudamiftahul362@gmail.com>
2023-05-10 15:33:19 +0700

    membuat class mahasiswa dengan bersifat public

--- pointerr/pointerr.cpp
+++ pointerr/pointerr.cpp
@@ -4,18 +4,29 @@ using namespace std;
 class mahasiswa {
 public:
 	int nim;
-	void showNIm();  //deklarasi method
+	void showNim();  //deklarasi method
 
 };
 
 
-void mahasiswa::showNIm() {  //implementasi method diluar class
+void mahasiswa::showNim() {  //implementasi method diluar class
 	cout << "no induk = " << nim << endl;
  }
 
 int main()
 {
 	mahasiswa mhs{ 1 };   //object mhs
-	mhs.showNIm();  //member acces operator
+	mhs.showNim();  //member acces operator
+
+	mahasiswa& ref = mhs;  //pointer refrences refMhs
+	ref.nim = 2;  //member access operator
+	mhs.showNim(); //
+
+	mahasiswa* pMhs = &mhs; //pointer references pMhs
+	pMhs->nim = 3;  //arrow operator
+	mhs.showNim();
+	system("pause");
+	return 0;
+
 	 
 }
