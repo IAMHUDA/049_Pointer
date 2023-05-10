067e5e490553810de4406be2b0939c2dadc22ca6
IAMHUDA <hudamiftahul362@gmail.com>
2023-05-10 15:02:02 +0700

    implementasi method diluar class

--- pointerr/pointerr.cpp
+++ pointerr/pointerr.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 class mahasiswa {
-	public;
+public:
 	int nim;
 	void showNIm();  //deklarasi method
 
 };
 
- 
+
+void mahasiswa::showNIm() {  //implementasi method diluar class
+	cout << "no induk = " << nim << endl;
+ }
